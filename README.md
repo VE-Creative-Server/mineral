05428e11868484211693b1581d27b705cca7f2b0
LminusSucksAtCoding <lminusmt@gmail.com>
2025-04-25 02:45:50 -0500

    added jade

---
+++ README.md
@@ -0,0 +1,6 @@
+# mineral ![Lua](https://img.shields.io/badge/lua-%232C2D72.svg?style=for-the-badge&logo=lua&logoColor=white)
+
+[![MT-LuaCheck CI](https://github.com/VE-Creative-Server/mineral/actions/workflows/luacheck.yml/badge.svg)](https://github.com/VE-Creative-Server/mineral/actions/workflows/luacheck.yml)
+
+This is mainly a mod for Drip_Knux (cdh).  currently just adds jade.
+jade texture done by Drip_Knux, based on technic marble
---
+++ init.lua
@@ -0,0 +1,17 @@
+core.register_node("mineral:jade", {
+    description = "Jade",
+    tiles = {"mineral_jade.png"},
+})
+
+
+if core.get_modpath("moreblocks") then
+  stairsplus:register_all("mineral", "jade", "mineral:jade", {
+    description="Jade",
+    groups={cracky=3, not_in_creative_inventory=1},
+    tiles={"mineral_jade.png"},
+  })
+end
+
+if core.get_modpath("technic_cnc") then
+  technic_cnc.register_all("mineral:jade", {cracky=3, not_in_creative_inventory=1}, {"mineral_jade.png"}, "Jade")
+end
